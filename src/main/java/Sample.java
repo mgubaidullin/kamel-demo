95c3451e7595d1574dcbde7f44edbe5e662d7e84
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2019-01-15 22:35:45 +0000

    initial

---
+++ src/main/java/Sample.java
@@ -0,0 +1,18 @@
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+public class Sample extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:tick")
+                .process(new Processor() {
+                    public void process(Exchange exchange) throws Exception {
+                        System.out.println("Hello Che !");
+                    }
+                })
+                .log("${headers}")
+                .setBody(constant("- HELLO KAMEL"))
+                .to("log:info?skipBodyLineSeparator=false");
+    }
+}
